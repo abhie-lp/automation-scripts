5b7e56768d5caf38b7229224723d69969ebf9c19
Abhishek Tiwari <abhie.lp@gmail.com>
2021-05-11 22:43:47 +0530

    Sleep for 20 seconds to counter sleep

--- work_apps/automate_work_apps.py
+++ work_apps/automate_work_apps.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from os import startfile, path
 from subprocess import run
 from time import sleep
@@ -14,52 +14,36 @@ OUTLOOK_LOCATION = config("OUTLOOK_LOCATION")
 
 NOTIFICATION = path.join(path.dirname(__file__), "notify_user.ps1")
 
+WORK_STARTED = False
+
 
 def notify_user(message) -> None:
     """Send the bubble notification for windows with message"""
     run(["powershell", NOTIFICATION, f'"{message}"'], shell=True)
 
 
-def sleep_seconds(today: datetime) -> int:
-    """Returns the number of seconds the script has to sleep"""
-    # Assuming that script is running between 12 AM to 9 AM
-    # Copy today's data
-    next_datetime = today.replace()
-    
-    # Check if script is running in off day or after work hours
-    if today.weekday() in (5, 6) or today.hour > 18:
-        # Increment 1 day in today and assign to next_datetime
-        next_datetime = today + timedelta(1)
-
-    # Replace next_datetime hour, minute and seconds to 9 o'clock
-    next_datetime = next_datetime.replace(hour=9, minute=0, second=0)
-
-    return int((next_datetime - today).total_seconds())
+def start_office_apps() -> None:
+    """Start the office apps like Chrome, Teams, Outlook"""
+    run(
+        ["start", "chrome", "--start-maximized",
+         GMAIL_LINK, CALENDAR_LINK, CHATS_LINK],
+        shell=True
+    )
+    startfile(TEAMS_LOCATION)
+    startfile(OUTLOOK_LOCATION)
 
 
 notify_user("Starting script to automate office apps.")
 
-
 current_datetime: datetime
 while current_datetime := datetime.today():
     if current_datetime.weekday() in (5, 6):
-        notify_user("No office today. XD")
-        sleep(sleep_seconds(current_datetime) + 20)
-        continue
+        pass
+    elif not WORK_STARTED and 21 <= current_datetime.hour <= 23:
+        start_office_apps()
+        WORK_STARTED = True
+    elif current_datetime.hour > 23:
+        WORK_STARTED = False
 
-    if 9 <= current_datetime.hour <= 18:
-        run(
-            ["start", "chrome", "--start-maximized",
-             GMAIL_LINK, CALENDAR_LINK, CHATS_LINK],
-            shell=True
-        )
-        startfile(TEAMS_LOCATION)
-        startfile(OUTLOOK_LOCATION)
-
-        # Sleep for next 12 hours
-        notify_user("Sleeping for next 12 hours")
-        sleep(43200)
-    else:
-        sleepy_seconds = sleep_seconds(current_datetime) + 20
-        notify_user(f"You are early. Sleeping for {sleepy_seconds} seconds")
-        sleep(sleepy_seconds)
+    # Sleep for 15 seconds and then check again.
+    sleep(20)
